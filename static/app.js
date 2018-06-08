8484c69b329a4a62d3f6f1044b8f6e2a1fc4b3fe
laptopuser <ces271@rutgers.edu>
2018-06-07 22:48:52 -0400

    final version with working optionChanged

--- static/app.js
+++ static/app.js
@@ -8,7 +8,7 @@ function init() {
         // Handle errors
         if (error) return console.log(error);
         
-        console.log(response)
+        //console.log(response)
         
         // Loop through response
         for(var i = 0 ; i <response.length; i++){
@@ -18,59 +18,67 @@ function init() {
         
         dropDownOption.innerHTML = item;
         
-        dropdown.append(dropDownOption);
-        
-    };
+        dropdown.append(dropDownOption);   
+        };
         //Create PIE chart with data from /samples/<sample> and /otu to display the top 10 samples.
 
-        console.log(response[0]);
+        //console.log(response[0]);
         var sampleURL = "../samples/" + response[0];
         
         d3.json(sampleURL, function(error,sampleResponse){
             // Handle errors
             if (error) return console.log(error);
             
-            topOtu = [];
-            topSamples = [];
+            var otuURL = "../otu";
+            d3.json(otuURL, function(error,otuResponse){
+                if (error) return console.log(error);
+                
+                
+                topOtu = [];
+                topSamples = [];
+                otuDescr = [];
 
-            for(var i = 0; i < 10; i++){
-                topOtu.push(sampleResponse[0].otu_ids[i]);
-                topSamples.push(sampleResponse[0].sample_values[i]);
-            };
-            //1. Use the OTU Description as the hovertext for the chart*/
+                for(var i = 0; i < 10; i++){
+                    topOtu.push(sampleResponse[0].otu_ids[i]);
+                    topSamples.push(sampleResponse[0].sample_values[i]);
+                    otuDescr.push(otuResponse[sampleResponse[0].otu_ids[i]]);
+                };
+                //console.log(topOtu)
+                //console.log(otuDescr)
 
-            pieData = [{
-                labels: topOtu,
-                values: topSamples,
-                type: 'pie' }];
-            var pieLoc = document.getElementById("pieChart");
-            Plotly.plot(pieLoc, pieData);
-            
-            //create bubble chart
-            bubbleData = [{
-                x: sampleResponse[0].otu_ids,
-                y: sampleResponse[0].sample_values,
-                mode: 'markers',
-                marker: {
-                    color: sampleResponse[0].otu_ids,
-                    size: sampleResponse[0].sample_values }}];
-            bubbleLayout = {
-                xaxis: {
-                    title: "OTU ID"
-                },
-                yaxis: {
-                    title: "Sample Values"
+                pieData = [{
+                    labels: topOtu,
+                    values: topSamples,
+                    type: 'pie',
+                    hovertext: otuDescr
+                 }];
+                var pieLoc = document.getElementById("pieChart");
+                Plotly.plot(pieLoc, pieData);
+                
+                var otuDescAll = [];
+                for(i=0;i<sampleResponse[0].otu_ids.length;i++){
+                    otuDescAll.push(otuResponse[sampleResponse[0].otu_ids[i]])
                 }
-            };
-            var bubbleLoc = document.getElementById("bubbleChart");
-            Plotly.plot(bubbleLoc, bubbleData, bubbleLayout);
-        });
+                //create bubble chart
+                bubbleData = [{
+                    x: sampleResponse[0].otu_ids,
+                    y: sampleResponse[0].sample_values,
+                    text: otuDescAll,
+                    mode: 'markers',
+                    marker: {
+                        color: sampleResponse[0].otu_ids,
+                        size: sampleResponse[0].sample_values }}];
+                bubbleLayout = {
+                    xaxis: {title: "OTU ID"},
+                    yaxis: {title: "Sample Values"}
+                };
+                var bubbleLoc = document.getElementById("bubbleChart");
+                Plotly.plot(bubbleLoc, bubbleData, bubbleLayout);
+        })}); //sample AND description d3.json
             /*Create a Bubble Chart that uses data from your routes /samples/<sample> 
 
-           2. -Use the OTU Description Data for the text values
-           3. -Use Plotly.restyle to update the chart whenever a new sample is selected*/
-        
-        
+
+           3. -Use Plotly.restyle to update the chart whenever a new sample is selected*/       
         var metadataURL = "../metadata/" + response[0];
 
         d3.json(metadataURL, function (error, metadataResponse){
@@ -83,14 +91,13 @@ function init() {
                 
                 contents.innerHTML = key + ": " + value;
                 contents.id = key;
-                meta.append(contents);
-                
+                meta.append(contents); 
               };
             
-        })
+        });
 
-})
-};
+}) //close paren for names d3.json
+}; //close paren for init
 
 //Use Plotly.restyle to update the chart whenever a new sample is selected
 function optionChanged(querySample) {
@@ -101,17 +108,49 @@ function optionChanged(querySample) {
     d3.json(sampleURL, function(error,sampleResponse){
         // Handle errors
         if (error) return console.log(error);
-        
-        topOtu = [];
-        topSamples = [];
 
-        for(var i = 0; i < 10; i++){
-            topOtu.push(sampleResponse[0].otu_ids[i]);
-            topSamples.push(sampleResponse[0].sample_values[i]);
-        };
-        Plotly.restyle(pieLoc, "labels", [topOtu]);
-        Plotly.restyle(pieLoc, "values", [topSamples]);
-    });
+        var otuURL = "../otu";
+        d3.json(otuURL, function(error,otuResponse){
+            if (error) return console.log(error);
+        
+            topOtu = [];
+            topSamples = [];
+            otuDescr = [];
+
+            for(var i = 0; i < 10; i++){
+                topOtu.push(sampleResponse[0].otu_ids[i]);
+                topSamples.push(sampleResponse[0].sample_values[i]);
+                otuDescr.push(otuResponse[sampleResponse[0].otu_ids[i]]);
+            };
+            Plotly.restyle(pieLoc, "labels", [topOtu]);
+            Plotly.restyle(pieLoc, "values", [topSamples]);
+            Plotly.restyle(pieLoc, "hovertext", [otuDescr]);
+    
+            //replot bubble chart
+            var otuDescAll = [];
+
+            for(i=0;i<sampleResponse[0].otu_ids.length;i++){
+                    otuDescAll.push(otuResponse[sampleResponse[0].otu_ids[i]])
+                };
+
+                //create bubble chart
+                bubbleData = [{
+                    x: sampleResponse[0].otu_ids,
+                    y: sampleResponse[0].sample_values,
+                    text: otuDescAll,
+                    mode: 'markers',
+                    marker: {
+                        color: sampleResponse[0].otu_ids,
+                        size: sampleResponse[0].sample_values }}];
+                bubbleLayout = {
+                    xaxis: {title: "OTU ID"},
+                    yaxis: {title: "Sample Values"}
+                };
+                var bubbleLoc = document.getElementById("bubbleChart");
+                Plotly.deleteTraces(bubbleLoc, 0);
+                Plotly.plot(bubbleLoc, bubbleData, bubbleLayout);
+        })});
+   
     var metadataURL = "../metadata/" + querySample;
 
     d3.json(metadataURL, function (error, metadataResponse){
@@ -131,6 +170,5 @@ function optionChanged(querySample) {
     
 
 };
-  
 
 init();
